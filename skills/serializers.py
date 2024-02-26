9afb54201039274ff17afca39d44384228806137
alim2709 <alimselemetow@gmail.com>
2024-02-26 22:10:22 +0200

    Implement SkillSerializer in skills/serializers.py file

---
+++ skills/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+
+from skills.models import Skill
+
+
+class SkillSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Skill
+        fields = (
+            "id",
+            "name",
+            "description",
+            "level",
+            "topics",
+        )
