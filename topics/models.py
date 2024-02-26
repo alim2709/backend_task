f9af322b3c1e2111bf61e4b8339ed8d5433ceb43
alim2709 <alimselemetow@gmail.com>
2024-02-26 20:11:26 +0200

    Add model UsefulLink & Topic in app topics/models.py

--- topics/models.py
+++ topics/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class UsefulLink(models.Model):
+    name = models.CharField(max_length=255)
+    link_url = models.URLField()
+
+    def __str__(self):
+        return self.name
+
+
+class Topic(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    pet_project_ideas = models.CharField(max_length=255)
+    useful_links = models.ForeignKey(
+        UsefulLink, on_delete=models.CASCADE, null=True, related_name="topics"
+    )
+
+    def __str__(self) -> str:
+        return f"Topic: {self.name}"
