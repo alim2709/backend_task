e11adba540bcc5fde08adfd8ef399fddc085ba56
alim2709 <alimselemetow@gmail.com>
2024-02-26 20:10:50 +0200

    Add model Skill in app skills/models.py

--- skills/models.py
+++ skills/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Skill(models.Model):
+    class LevelChoices(models.TextChoices):
+        BEGINNER = "Beginner"
+        INTERMEDIATE = "Intermediate"
+        ADVANCED = "Advanced"
+
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    level = models.CharField(choices=LevelChoices, default="BEGINNER", max_length=255)
+    topics = models.ManyToManyField("Topic", related_name="skills")
+
+    def __str__(self):
+        return self.name
