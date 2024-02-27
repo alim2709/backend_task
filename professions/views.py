7bea0e7f538c58b79dc6805b22cc790f5403cb7a
alim2709 <alimselemetow@gmail.com>
2024-02-28 00:54:29 +0200

    Fix n+1 problem in ProfessionViewSet in professions/views.py file

--- professions/views.py
+++ professions/views.py
@@ -1,11 +1,16 @@
 from rest_framework import viewsets
 
 from professions.models import Profession
-from professions.serializers import ProfessionSerializer
+from professions.serializers import ProfessionSerializer, ProfessionListSerializer
 
 
 class ProfessionViewSet(viewsets.ModelViewSet):
     """Endpoint for CRUD operations with professions"""
 
-    queryset = Profession.objects.all()
+    queryset = Profession.objects.all().prefetch_related("skills__topics__useful_links")
     serializer_class = ProfessionSerializer
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return ProfessionListSerializer
+        return ProfessionSerializer
