886ca71717071da5f5ed457fa8a7020484001236
alim2709 <alimselemetow@gmail.com>
2024-02-27 20:18:03 +0200

    Add changes to models in topic/models.py file

--- topics/models.py
+++ topics/models.py
@@ -4,6 +4,9 @@ from django.db import models
 class UsefulLink(models.Model):
     name = models.CharField(max_length=255)
     link_url = models.URLField()
+    topic = models.ForeignKey(
+        "Topic", on_delete=models.CASCADE, related_name="useful_links"
+    )
 
     def __str__(self):
         return self.name
@@ -13,9 +16,6 @@ class Topic(models.Model):
     name = models.CharField(max_length=255)
     description = models.CharField(max_length=255)
     pet_project_ideas = models.CharField(max_length=255)
-    useful_links = models.ForeignKey(
-        UsefulLink, on_delete=models.CASCADE, null=True, related_name="topics"
-    )
 
     def __str__(self) -> str:
         return f"Topic: {self.name}"
