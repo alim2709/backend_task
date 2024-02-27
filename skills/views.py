d2d8a9e3e6e26c0e7b4d92ec38dc62a3544c9394
alim2709 <alimselemetow@gmail.com>
2024-02-28 00:56:41 +0200

    Fix n+1 problem in SkillViewSet in skills/views.py file

--- skills/views.py
+++ skills/views.py
@@ -1,11 +1,16 @@
 from rest_framework import viewsets
 
 from skills.models import Skill
-from skills.serializers import SkillSerializer
+from skills.serializers import SkillSerializer, SkillListSerializer
 
 
 class SkillViewSet(viewsets.ModelViewSet):
     """Endpoint for CRUD operations with skills"""
 
-    queryset = Skill.objects.all()
+    queryset = Skill.objects.all().prefetch_related("topics__useful_links")
     serializer_class = SkillSerializer
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return SkillListSerializer
+        return SkillSerializer
