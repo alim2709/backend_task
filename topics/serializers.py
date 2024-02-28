590a04523ab9df83312e24cd0cc7985fbb997ba5
alim2709 <alimselemetow@gmail.com>
2024-02-29 00:22:18 +0200

    Update method in TopicSerializer

--- topics/serializers.py
+++ topics/serializers.py
@@ -30,10 +30,19 @@ class TopicSerializer(serializers.ModelSerializer):
             UsefulLink.objects.create(topic=topic, **useful_link_data)
         return topic
 
+    def update(self, instance, validated_data):
+        useful_links_data = validated_data.pop("useful_links", None)
+        if useful_links_data is not None:
+            instance.useful_links.all().delete()
+            for useful_link_data in useful_links_data:
+                UsefulLink.objects.create(topic=instance, **useful_link_data)
+
+        return super().update(instance, validated_data)
+
 
 class TopicListSerializer(TopicSerializer):
-    useful_links = UsefulLinkListSerializer(many=True, read_only=False)
+    useful_links = UsefulLinkListSerializer(many=True, read_only=True)
 
     class Meta:
         model = Topic
-        fields = ("name", "description", "pet_project_ideas", "useful_links")
+        fields = ("id", "name", "description", "pet_project_ideas", "useful_links")
